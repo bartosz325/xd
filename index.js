aaeb2d6c6cb09ef46849a6d3f4398dd28fb02912
bartosz325 <bpiszczek97@gmail.com>
2017-10-16 13:40:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express')
 var app = express()
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const dom = new JSDOM(`<p>Hello world</p>`);
 
 app.set('port', (process.env.PORT || 80))
 app.use(express.static(__dirname + '/public'))
@@ -8,13 +11,10 @@ app.get('/', function(request, response) {
   var a='bartosz';
   a=a.substr(1,3);
   var b='<html><head><title>dasdas</title></head><body>Hello World!'+a+' xd</body></html>';
-  response.send(b);
+  c=dom.window.document.querySelector("p").textContent);
+  response.send(b+'------'+c);
 })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const dom = new JSDOM(`<p>Hello world</p>`);
-console.log(dom.window.document.querySelector("p").textContent);
