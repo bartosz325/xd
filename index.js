f9c666e3d676533144eb7722f69b3a43e80d423e
bartosz325 <bpiszczek97@gmail.com>
2017-10-16 13:33:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.listen(app.get('port'), function() {
 
 var http = require('http');
 
+//create a server object:
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}).listen(8080);
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
