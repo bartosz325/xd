f433076fb4423f43f84ab4caadeec013ec55faca
bartosz325 <bpiszczek97@gmail.com>
2017-10-17 23:13:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-    req("http://ionicabizau.net/", function (err, body) {
-    response.send(err || body); // Print out the HTML
-    });
-    
+    var dasd=req("http://ionicabizau.net/");
+    response.send(dasd); // Print out the HTML   
 })
 
 app.listen(app.get('port'), function() {
