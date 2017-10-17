31b5ecc382f9de62e6ab884f804437685fd55844
bartosz325 <bpiszczek97@gmail.com>
2017-10-17 23:11:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 var req = require("tinyreq");
 
-app.set('port', (process.env.PORT || 80))
+app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
