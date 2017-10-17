433cb41dfa75157a5c873502b24430cc5d8e826c
bartosz325 <bpiszczek97@gmail.com>
2017-10-17 23:15:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var express = require('express')
 var app = express()
-var req = require("tinyreq");
+var request = require("tinyreq");
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-    var dasd=req("http://ionicabizau.net/");
-    response.send(dasd); // Print out the HTML   
-})
+request('myurl', function(err, body, xhr){
+	if (err){
+		//request ended badly
+	}
+	else{
+		//rejoice!
+	}
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
