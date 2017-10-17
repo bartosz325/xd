0956fb7cdc4c3c467e19c6584b656e31c46a7b7e
bartosz325 <bpiszczek97@gmail.com>
2017-10-17 23:30:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var express = require('express')
 var app = express()
+var request = require('tinyreq');
+var app2 = request()
+
+request("http://ionicabizau.net/", function (err, body) {
+    console.log(err || body); // Print out the HTML
+});
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
