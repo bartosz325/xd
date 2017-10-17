7da207ec1b9ecc4487dcbd4316f1e87ad91847ea
bartosz325 <bpiszczek97@gmail.com>
2017-10-17 22:53:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var app = angular.module('myApp', []);
 
 app.set('port', (process.env.PORT || 80))
 app.use(express.static(__dirname + '/public'))
@@ -11,3 +12,9 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+app.controller('myCtrl', function($scope, $http) {
+    $http.get("welcome.htm").then(function (response) {
+        $scope.myWelcome = response.data;
+    });
+});
