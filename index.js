530012724bef64bb14702707437a1b77cd83d4c6
bartosz325 <bpiszczek97@gmail.com>
2017-10-18 00:28:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express');
-var tinyurl = require('tinyurl');
+var request = require('request');
 var app = express();
 
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
-
+request('https://urlxray.expeditedaddons.com/?api_key=' + process.env.URLXRAY_API_KEY + '&fetch_content=false&url=http%3A%2F%2Fwww.wikipedia.org', function (error, response, body) {
+  console.log('Status:', response.statusCode);
+  console.log('Headers:', JSON.stringify(response.headers));
+  console.log('Response:', body);
+});
 app.get('/', function(request, response) {
   response.send('Hello World!');
 })
